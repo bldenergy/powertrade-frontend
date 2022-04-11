34a9e862f61a44e75a5ca5035ca1e07ccb3d5e83
Claude Vernet Michel <claudevernetmichel22@gmail.com>
2022-04-11 16:34:15 +0800

    Multiple browsers

--- cypress/integration/home_spec.ts
+++ cypress/integration/home_spec.ts
@@ -1,5 +1,5 @@
-describe('My First Test', () => {
-  it('Visits the Kitchen Sink', () => {
+describe('Switch to Scheduling page', () => {
+  it('Visit Scheduling page', () => {
     cy.visit('/');
     cy.contains('表格').click();
     cy.url().should('include', '/scheduling');
