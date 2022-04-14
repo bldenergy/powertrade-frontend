c40547e4d4c1de7e3a7473026c00d29e0b7b6a0a
Claude Vernet Michel <claudevernetmichel22@gmail.com>
2022-04-14 15:53:45 +0800

    Code Coverage

---
+++ instrumented/footer.tsx
@@ -0,0 +1,14 @@
+import styles from '../styles/footer.module.css';
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <a
+        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer">
+        Powered by BLD Energy
+      </a>
+    </footer>
+  );
+}
--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa');
 const runtimeCaching = require('next-pwa/cache');
+const prod = process.env.NODE_ENV === 'production';
 
 module.exports = withPWA({
   reactStrictMode: true,
@@ -15,5 +16,6 @@ module.exports = withPWA({
   pwa: {
     dest: 'public',
     runtimeCaching,
+    disable: prod ? false : true,
   },
 });
