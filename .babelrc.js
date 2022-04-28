8785cf79b99d916e36fc6ea83c4f43f8bb1bba6c
Claude Vernet Michel <claudevernetmichel22@gmail.com>
2022-04-28 13:27:02 +0800

    fix: Codecov instrument

---
+++ .babelrc.js
@@ -0,0 +1,9 @@
+const shouldInstrumentCode = true
+console.log('shouldInstrumentCode', shouldInstrumentCode)
+
+module.exports = {
+  presets: ['next/babel'],
+  plugins: shouldInstrumentCode ? ['istanbul'] : []
+}
+
+console.dir(module.exports, { depth: null })
