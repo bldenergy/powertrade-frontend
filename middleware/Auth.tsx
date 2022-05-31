b67913d495a5c48e14a78eae46d96cffbc79c47e
Claude Vernet Michel <claudevernetmichel22@gmail.com>
2022-05-31 16:20:18 +0800

    Next-auth redirecting to bldenergy-myaccount (#32)
    * feat: added nextauth
    
    * feat: use next-auth to redirect to bldenergy-myaccount

--- middleware/Auth.tsx
+++ middleware/Auth.tsx
@@ -1,35 +1,37 @@
-import { signIn, useSession } from "next-auth/react";
-import { ReactNode, useEffect } from "react";
-import Layout from "../components/Layout/layout";
-import { Spinner } from "@chakra-ui/react";
+import styles from '../styles/shared.module.css'
+import { Spinner } from '@chakra-ui/react'
+import { signIn, useSession } from 'next-auth/react'
+import { ReactNode, useEffect } from 'react'
 
 type AuthProps = {
-  children: ReactNode;
-};
-
-// Based on: https://github.com/nextauthjs/next-auth/issues/1210#issuecomment-782630909
+  children: ReactNode
+}
 export default function Auth({ children }: AuthProps) {
-  const { data: session, status } = useSession();
-  const isUser = !!session?.user;
+  const { data: session, status } = useSession()
+  const isUser = !!session?.user
 
   useEffect(() => {
-    if (status === "loading") return; // Do nothing while loading
-    if (!isUser) signIn(); // If not authenticated, force log in
-  }, [isUser, status === "loading"]);
+    if (status === 'loading') return // Do nothing while loading
+    if (!isUser) signIn('kratos-hydra') // If not authenticated, force log in
+  }, [isUser, status])
 
   if (isUser) {
-    return <Layout>{children}</Layout>;
+    return <>{children}</>
   }
 
   // Session is being fetched, or no user.
   // If no user, useEffect() will redirect.
   return (
-    <Spinner
-            thickness="4px"
-            speed="0.65s"
-            emptyColor="gray.200"
-            color="#088be0"
-            size="xl"
-          />
-  );
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <Spinner
+          thickness="4px"
+          speed="0.65s"
+          emptyColor="gray.200"
+          color="#088be0"
+          size="xl"
+        />
+      </main>
+    </div>
+  )
 }
--- pages/power-consumption.tsx
+++ pages/power-consumption.tsx
@@ -9,104 +9,32 @@ import en from '../locales/en'
 import zh from '../locales/zh'
 
 const Consumption: NextPage = () => {
-  let token: any
-  if (typeof window !== 'undefined') {
-    token = localStorage.getItem('Xjdfnd') || undefined
-  }
   const router = useRouter()
   const { locale } = router
   const translate = locale === 'en' ? en : zh
-  const [] = useState<string>(
-    'No valid Ory Session was found.\nPlease sign in to receive one.'
-  )
-  const [] = useState<boolean>(false)
-  const [loading] = useState(true)
-
-  // Verify wheter token exists, if yes then access this page, if no : check if Kratos session exits, esle redirect to login
-  useEffect(() => {
-    // if (token === undefined) {
-    //   checkLogout(
-    //     hasSession,
-    //     `${serverProps.ory_hydra_public_url}/oauth2/sessions/logout`
-    //   )
-    // } else {
-    //   try {
-    //     const decodeToken: any = jwt_decode(token)
-    //     if (!decodeToken.hasOwnProperty('client_id')) {
-    //       checkLogout(
-    //         hasSession,
-    //         `${serverProps.ory_hydra_public_url}/oauth2/sessions/logout`
-    //       )
-    //     }
-    //   } catch (err) {
-    //     checkLogout(
-    //       hasSession,
-    //       `${serverProps.ory_hydra_public_url}/oauth2/sessions/logout`
-    //     )
-    //   }
-    // }
-
-    // const powerUsageService = new PowerUsageServiceClient(
-    //   `http://${window.location.hostname}:8081`,
-    //   null,
-    //   null
-    // )
-    // var request = new GetPowerUsageRequest()
-    // const call = powerUsageService.getPowerUsage(
-    //   request,
-    //   { 'custom-header-1': 'value1' },
-    //   (err: grpcWeb.RpcError, response: PowerUsage) => {
-    //     console.log(err)
-    //     console.log(response.toObject())
-    //   }
-    // )
-    // call.on('status', (status: grpcWeb.Status) => {
-    //   console.log(status)
-    // })
-
-    // // var get60TicksPowerUsageRequest = new Get60TicksPowerUsageRequest()
-    // // const streamCall = powerUsageService.get60TicksPowerUsage(get60TicksPowerUsageRequest, {});
-    // // streamCall.on('data', (resp: PowerUsage) => {
-    // //   console.log(resp);
-    // // });
-    // // streamCall.on('end', () => {
-    // //   console.log("It's the end");
-    // // });
-  })
 
   return (
     <div className={styles.container}>
       <HeadComponent title="BLD PowerTrade - Power Consumption" />
       <main className={styles.main}>
-        {!loading ? (
-          <>
-            <h1 className={styles.title}>{translate.powerconsumption.title}</h1>
-            <p className={styles.description}>
-              {translate.powerconsumption.subTitle}
-            </p>
-          </>
-        ) : (
-          <Spinner
-            thickness="4px"
-            speed="0.65s"
-            emptyColor="gray.200"
-            color="#088be0"
-            size="xl"
-          />
-        )}
+        <>
+          <h1 className={styles.title}>{translate.powerconsumption.title}</h1>
+          <p className={styles.description}>
+            {translate.powerconsumption.subTitle}
+          </p>
+        </>
       </main>
     </div>
   )
 }
 
-
 // This gets called on every request
-export const getServerSideProps: GetServerSideProps = async () => {
-  return {
-    props: {
-      ory_hydra_public_url: process.env.ORY_HYDRA_PUBLIC_URL
-    }
-  }
-}
+// export const getServerSideProps: GetServerSideProps = async () => {
+//   return {
+//     props: {
+//       ory_hydra_public_url: process.env.ORY_HYDRA_PUBLIC_URL
+//     }
+//   }
+// }
 
 export default Consumption
