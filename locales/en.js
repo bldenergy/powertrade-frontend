8cc26b68a383376ac19f16d218ce3bec6a67e0ca
Claude Vernet Michel <claudevernetmichel22@gmail.com>
2022-06-23 09:31:20 +0800

    feat: add setting page (#49)

--- locales/en.js
+++ locales/en.js
@@ -22,5 +22,8 @@ export default {
   account: {
     title: 'Account',
     logout: 'Logout'
+  },
+  settings: {
+    title: 'Settings'
   }
 }
---
+++ pages/settings.tsx
@@ -0,0 +1,27 @@
+import styles from '../styles/shared.module.css'
+import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
+import React, { useState } from 'react'
+
+import HeadComponent from '../components/head'
+import en from '../locales/en'
+import zh from '../locales/zh'
+
+const Settings: NextPage = () => {
+  const router = useRouter()
+  const { locale } = router
+  const translate = locale === 'en' ? en : zh
+  return (
+    <div className={styles.container}>
+      <HeadComponent title="BLD PowerTrade - Scheduling..." />
+      <main className={styles.main}>
+        <>
+          <h1 className={styles.title}>{translate.settings.title}</h1>
+          {/* <p className={styles.description}>{translate.settings.subTitle}</p> */}
+        </>
+      </main>
+    </div>
+  )
+}
+
+export default Settings
